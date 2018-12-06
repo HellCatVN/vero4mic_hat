9980532714545be107c2e735da5b2b42f513732a
HellCatVN <hellcatvn@gmail.com>
2018-12-07 00:01:12 +0700

    Move VeroFile

--- vero_led_pattern.py
+++ vero_led_pattern.py
@@ -21,7 +21,7 @@ class VeroLedPattern(object):
 
     def wakeup(self, direction=0):
         position = int((direction + 15) / (360 / self.pixels_number)) % self.pixels_number
-
+        print("postion:"+ str(position))
         pixels = [0, 0, 0, 24] * self.pixels_number
         pixels[position * 4 + 2] = 48
 
